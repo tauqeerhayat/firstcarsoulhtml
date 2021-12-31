bd2e472df7299dd9dae997c30894430b5d5a484a
tauqeerhayat <tauqeerhayat10@gmail.com>
2022-01-01 04:03:49 +0500

    2commit

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fs-2 fw-bold">
+    <div class="container-fluid">
+        <a class="navbar-brand fs-1 fw-bold" href="#"><h1 style="color: #D55161">OnlineShop</h1></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#"><h1 style="color: #BDC8C9">Home</h1></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><h1 style="color: #BDC8C9">New Arrival</h1></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" tabindex="-1" aria-disabled="true"><h1 style="color: #BDC8C9">OnlineShop</h1></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Category
+                    </a>
+                    <ul class="dropdown-menu" aria-labelled-by="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Mens</a></li>
+                        <li><a class="dropdown-item" href="#">Woman</a></li>
+                        <li><a class="dropdown-item" href="#">Other's</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn text-white" type="submit" style="background: #D55161; align-content: center"><h1
+                        style="font-size: large;">Search</h1></button>
+            </form>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/layouts/slide.blade.php
@@ -0,0 +1,55 @@
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="10000">
+            <img src="https://devrant.com/rants/186784/image.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+            <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="https://pbs.twimg.com/media/E03_zBVVkAYLdHn.png" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+
+
+
+
+
+
+
+
+
+{{--<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">--}}
+{{--    <div class="carousel-inner">--}}
+{{--        <div class="carousel-item active">--}}
+{{--            <img src="../public/image/01.jpg" class="d-block w-100" alt="...">--}}
+{{--        </div>--}}
+{{--        <div class="carousel-item">--}}
+{{--            <img src="./public/image/02.jpg" class="d-block w-100" alt="...">--}}
+{{--        </div>--}}
+{{--        <div class="carousel-item">--}}
+{{--            <img src="./public/image/03.jpg" class="d-block w-100" alt="...">--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">--}}
+{{--        <span class="carousel-control-prev-icon" aria-hidden="true"></span>--}}
+{{--        <span class="visually-hidden">Previous</span>--}}
+{{--    </button>--}}
+{{--    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">--}}
+{{--        <span class="carousel-control-next-icon" aria-hidden="true"></span>--}}
+{{--        <span class="visually-hidden">Next</span>--}}
+{{--    </button>--}}
+{{--</div>--}}
